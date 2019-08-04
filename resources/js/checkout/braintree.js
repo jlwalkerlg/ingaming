6c75c47c252fb964dcc0fba23b2f9a672d10b2e7
Jordan Walker <walker.jlg@gmail.com>
2019-08-04 21:02:55 +0100

    Remove console logs in checkout

--- resources/js/checkout/braintree.js
+++ resources/js/checkout/braintree.js
@@ -6,36 +6,30 @@ import dropin from 'braintree-web-drop-in';
 const form = document.querySelector('#checkoutForm');
 const clientToken = form.dataset.clientToken;
 
-console.log(dropin);
-console.log(dropin.create);
-
-console.log(
-  dropin.create(
-    {
-      authorization: clientToken,
-      selector: '#btDropin',
-      paypal: {
-        flow: 'vault',
-      },
+dropin.create(
+  {
+    authorization: clientToken,
+    selector: '#btDropin',
+    paypal: {
+      flow: 'vault',
     },
-    (err, instance) => {
-      if (err) {
-        console.log('Create Error', err);
-        return;
-      }
-      console.log(instance, document.querySelector('#btDropin'));
-      form.addEventListener('submit', e => {
-        e.preventDefault();
-        instance.requestPaymentMethod((err, payload) => {
-          if (err) {
-            console.log('Request Payment Method Error', err);
-            return;
-          }
-          // Add the nonce to the form and submit.
-          document.querySelector('#nonce').value = payload.nonce;
-          form.submit();
-        });
-      });
+  },
+  (err, instance) => {
+    if (err) {
+      console.log('Create Error', err);
+      return;
     }
-  )
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      instance.requestPaymentMethod((err, payload) => {
+        if (err) {
+          console.log('Request Payment Method Error', err);
+          return;
+        }
+        // Add the nonce to the form and submit.
+        document.querySelector('#nonce').value = payload.nonce;
+        form.submit();
+      });
+    });
+  }
 );
