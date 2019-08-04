8e26131b3aa7c359c1cacbef5d391df0148b93c4
Jordan Walker <walker.jlg@gmail.com>
2019-08-04 19:49:18 +0100

    Braintree checkout js

--- resources/js/checkout/braintree.js
+++ resources/js/checkout/braintree.js
@@ -1,34 +1,34 @@
-import "../../sass/style.sass";
-import "../modules/siteNav";
+import '../../sass/style.sass';
+import '../modules/siteNav';
 
-import dropin from "braintree-web-drop-in";
+import dropin from 'braintree-web-drop-in';
 
-const form = document.querySelector("#checkoutForm");
+const form = document.querySelector('#checkoutForm');
 const clientToken = form.dataset.clientToken;
-const amount = document.querySelector("#subtotal");
 
 dropin.create(
   {
     authorization: clientToken,
-    selector: "#btDropin",
+    selector: '#btDropin',
     paypal: {
-      flow: "vault"
-    }
+      flow: 'vault',
+    },
   },
   (err, instance) => {
     if (err) {
-      console.log("Create Error", err);
+      console.log('Create Error', err);
       return;
     }
-    form.addEventListener("submit", e => {
+    console.log(instance, document.querySelector('#btDropin'));
+    form.addEventListener('submit', e => {
       e.preventDefault();
       instance.requestPaymentMethod((err, payload) => {
         if (err) {
-          console.log("Request Payment Method Error", err);
+          console.log('Request Payment Method Error', err);
           return;
         }
         // Add the nonce to the form and submit.
-        document.querySelector("#nonce").value = payload.nonce;
+        document.querySelector('#nonce').value = payload.nonce;
         form.submit();
       });
     });
